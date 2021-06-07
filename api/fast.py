f5b8cc712ea5f02cf8b097e7c095351a39957629
Amine Aboufirass <amine.aboufirass@gmail.com>
2021-06-07 12:24:49 +0200

    adding basic content for api functionality

--- api/fast.py
+++ api/fast.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
 
 app = FastAPI()
 
@@ -10,3 +11,16 @@ app.add_middleware(
     allow_methods=["*"],  # Allows all methods
     allow_headers=["*"],  # Allows all headers
 )
+
+@app.get("/")
+def index():
+    return {"greeting": "Hello world"}
+
+@app.get("/predict")
+def predict(youtube_link):
+    # create a datetime object from the user provided datetime
+
+    return {'mixed_audio': 'test'}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
