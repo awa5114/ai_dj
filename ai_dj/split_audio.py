67e157d90922a751bb4453ea92eca9dadbeccb12
jvanleersum <75987870+jvanleersum@users.noreply.github.com>
2021-06-08 12:34:19 +0200

    Completed basic 2 stems mixing class

--- ai_dj/split_audio.py
+++ ai_dj/split_audio.py
@@ -1,24 +1,19 @@
 from spleeter.separator import Separator
+from ai_dj.params import DOWNLOADED_FOLDER, SPLIT_DATA_FOLDER
 
-class SpleeterSeparator:
-    def __init__(self, stems=4):
-        self.separator = Separator(f'spleeter:{stems}stems')
+class SpleeterSeparator():
+    def __init__(self, file, stems=4):
+        self.file = file
+        #self.separator = Separator(f'spleeter:{str(stems)}stems')
+        self.separator = Separator('spleeter:4stems')
     
-    def split_song(self, file, stems=4)
-        separator.separate_to_file(filename, 'Downloads/')
-        split_song(file)
-
-    song_1 = "64 Ways (Dam Swindle's 65th Way Dub) feat. Mayer Hawthorne-SKUk9RUacDQ.wav"
-    tempo_1, beat_frames_1, beat_times_1 = get_BPM(song_1)
-
-
-
-    song_2 = "Kygo, Sasha Sloan - I'll Wait (Lyric Video)-ogv284C4W30.wav"
-    tempo_2, beat_frames_2, beat_times_2 = get_BPM(song_2)
-
-
-    sound1 = AudioSegment.from_file("Downloads/64 Ways (Dam Swindle's 65th Way Dub) feat. Mayer Hawthorne-SKUk9RUacDQ/drums.wav")
-    sound2 = AudioSegment.from_file("Downloads/Kygo, Sasha Sloan - I'll Wait (Lyric Video)-ogv284C4W30/other.wav")
-    combined = sound1.overlay(sound2)
-
-    combined.export("../ai_dj/data/64_kygo_combined2.wav", format='wav')
+    def split_song(self):
+        #self.separator.separate_to_file(self.file, SPLIT_DATA_FOLDER)
+        self.separator.separate_to_file(self.file, "/Users/judithvanleersum/code/jvanleersum/ai_dj/ai_dj/data/split_audio_files/")
+    
+## Test ##
+#file = f'{DOWNLOADED_FOLDER}/Two Scoops-Q77vdqA0hnM.wav'
+file = "/Users/judithvanleersum/code/jvanleersum/ai_dj/ai_dj/data/downloaded_music/Two Scoops-Q77vdqA0hnM.wav"
+separator = SpleeterSeparator(file)
+separator.split_song()
+## Test ##
