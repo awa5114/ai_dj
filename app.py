453daf7298e80984fa06d91042a822ee69be45b8
Willem Sickinghe <sickinghe.willem@gmail.com>
2021-06-09 14:33:26 +0200

    working audio on streamlit

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
+from numpy.lib.shape_base import _make_along_axis_idx
 import streamlit as st
 
+#import from gcp bucket
+import youtube_dl
+import audioread
 import numpy as np
 import pandas as pd
+
+mix = '/Users/willemsickinghe/Downloads/Lonely (Original Mix).mp3'
 
 st.markdown("""# DJ for dummies
 ## Paste the Youtube link of your song
@@ -12,15 +18,19 @@ df = pd.DataFrame({
           'second column': np.arange(10, 101, 10)
         })
 
-# this slider allows the user to select a number of lines
-# to display in the dataframe
-# the selected value is returned by st.slider
-line_count = st.slider('Select a line count', 1, 10, 3)
-
+#line_count = st.slider('Select a line count', 1, 10, 3)
 # and used in order to select the displayed lines
-head_df = df.head(line_count)
+#head_df = df.head(line_count)
 
-head_df
+#head_df
+
+import streamlit as st
+
+st.audio(mix, format='audio/wav', start_time=0)
+
+audio_file = open(mix, 'rb')
+audio_bytes = audio_file.read()
+st.audio(audio_bytes, format='audio/wav')
 
 # Procfile
 #web: pip install . -U && ai_dj-run
