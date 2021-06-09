5d554f5cff71996db0c129dc1917d7bb20f54142
jvanleersum <75987870+jvanleersum@users.noreply.github.com>
2021-06-09 10:06:39 +0200

    Updated split_audio

--- ai_dj/split_audio.py
+++ ai_dj/split_audio.py
@@ -1,6 +1,5 @@
 from spleeter.separator import Separator
 from ai_dj.params import DOWNLOADED_FOLDER, SPLIT_DATA_FOLDER
-from demucs
 
 class SpleeterSeparator():
     def __init__(self, file, stems=4):
@@ -10,15 +9,15 @@ class SpleeterSeparator():
     
     def split_song(self):
         #self.separator.separate_to_file(self.file, SPLIT_DATA_FOLDER)
-        self.separator.separate_to_file(self.file, "/Users/judithvanleersum/code/jvanleersum/ai_dj/ai_dj/data/split_audio_files/")
+        self.separator.separate_to_file(self.file, SPLIT_DATA_FOLDER)
     
-class DemucsSeparator():
-    def __init__(self, file):
-        self.file = file
+# class DemucsSeparator():
+#     def __init__(self, file):
+#         self.file = file
         
 ## Test ##
 #file = f'{DOWNLOADED_FOLDER}/Two Scoops-Q77vdqA0hnM.wav'
-file = "/Users/judithvanleersum/code/jvanleersum/ai_dj/ai_dj/data/downloaded_music/Two Scoops-Q77vdqA0hnM.wav"
+file = f"{DOWNLOADED_FOLDER}/Two Scoops-Q77vdqA0hnM.wav"
 separator = SpleeterSeparator(file)
 separator.split_song()
 ## Test ##
