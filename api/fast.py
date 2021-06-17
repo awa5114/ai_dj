a602bd25c12543f92c19894f3dfa52a259925701
Amine Aboufirass <amine.aboufirass@gmail.com>
2021-06-17 21:09:02 +0200

    fixing api

--- api/fast.py
+++ api/fast.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from ai_dj import params
 from google.cloud import storage
-import soundfile
 import io
 
 app = FastAPI()
@@ -17,17 +17,15 @@ app.add_middleware(
 
 
 @app.get("/")
-def index(filename, start, stop):
+def index(filename):
+    #?filename=1019315 Guido Sava - Fever (Original Mix).wav
     storage_client = storage.Client()
     bucket = storage_client.bucket(params.BUCKET_NAME)
-    blob = bucket.blob(f'data/audio_wav/{filename}')
-    blob = blob.download_as_string()
+    blob = bucket.blob(f'data/audio_wav/1019315 Guido Sava - Fever (Original Mix).wav')
+    bts = blob.download_as_bytes()
+    stream = io.BytesIO(bts)
 
-    data, sr = soundfile.read(io.BytesIO(blob), start=int(start), stop=int(stop))
-    return {
-        "data":data[:,0].tolist(),
-        "sr":sr
-        }
+    return StreamingResponse(stream, media_type="audio/wav")
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
